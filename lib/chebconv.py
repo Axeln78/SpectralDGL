ca8002f54b8bba603adcaf1df8d1e604ac5d66a3
Axeln78 <axeln78@gmail.com>
2020-04-22 16:18:15 +0800

    Final version of the ChebConv

--- lib/chebconv.py
+++ lib/chebconv.py
@@ -4,19 +4,21 @@ import torch.nn.functional as F
 
 import dgl
 import dgl.function as fn
-'''
+
+
 class NodeApplyModule(nn.Module):
     """Update the node feature hv with ReLU(Whv+b)."""
-    def __init__(self, in_feats, out_feats, activation, bias=True):
+
+    def __init__(self, in_feats, out_feats, k, activation, bias=True):
         super(NodeApplyModule, self).__init__()
-        self.linear = nn.Linear(in_feats, out_feats, bias)
+        self.linear = nn.Linear(k * in_feats, out_feats, bias)
         self.activation = activation
 
     def forward(self, node):
         h = self.linear(node.data['h'])
-        h = self.activation(h)
+        if self.activation:
+            h = self.activation(h)
         return {'h': h}
-'''
 
 
 class Cheb_Conv(nn.Module):
@@ -29,6 +31,8 @@ class Cheb_Conv(nn.Module):
         Number of output features.
     k : int
         Chebyshev filter size.
+    activation : function, optional
+        Activation function.
     bias : bool, optional
         If True, adds a learnable bias to the output. Default: ``True``.
     """
@@ -37,37 +41,45 @@ class Cheb_Conv(nn.Module):
                  in_feats,
                  out_feats,
                  k,
+                 activation=F.relu,
                  bias=True):
         super(Cheb_Conv, self).__init__()
         self._k = k
         self._in_feats = in_feats
         self._out_feats = out_feats
-        self.fc = nn.Linear(in_feats * k, out_feats, bias)
-        # self.fc = NodeApplyModule(
-        #    in_feats*k, out_feats, activation=F.relu)
+        self.activation = activation
+        self.apply_mod = NodeApplyModule(
+            in_feats,
+            out_feats,
+            k=self._k,
+            activation=self.activation)
 
     def forward(self, graph, signal, lambda_max=None):
         """
         Parameters
         ----------
-        graph: DGLGraph
-        signal: [Nxf]
-            Signal of dimension f over all nodes N
-        k : int
-            Chebyshev filter size.
-        lambda_max : float, optional
-            Largest eigonvalue
+        graph : DGLGraph
+            The graph.
+        feat : torch.Tensor [Nxf]
+            The input feature of shape :math:`(N, D_{in})` where :math:`D_{in}`
+            is size of input feature, :math:`N` is the number of nodes.
+        lambda_max : list or tensor or None, optional.
+            A list(tensor) with length :math:`B`, stores the largest eigenvalue
+            of the normalized laplacian of each individual graph in ``graph``,
+            where :math:`B` is the batch size of the input graph. Default: None.
+            If None, this method would compute the list by calling
+            ``dgl.laplacian_lambda_max``.
+
+        Returns
+        -------
+        torch.Tensor
+            The output feature of shape :math:`(N, D_{out})` where :math:`D_{out}`
+            is size of output feature.
         """
 
-        def lin_layer(nodes):
-            """Update the node feature hv with ReLU(Whv+b)."""
-            h = self.fc(nodes.data['h'])
-            h = F.relu(h)
-            return {'h': h}
-
-        def unnLaplacian(signal, D_sqrt, graph):
+        def unnLaplacian(feat, D_sqrt, graph):
             """Operation D^-1/2 A D^-1/2, the DGL way."""
-            graph.ndata['h'] = signal * D_sqrt
+            graph.ndata['h'] = feat * D_sqrt
             graph.update_all(fn.copy_u('h', 'm'), fn.sum('m', 'h'))
             return graph.ndata.pop('h') * D_sqrt
 
@@ -96,7 +108,6 @@ class Cheb_Conv(nn.Module):
             # X_1(f)
             if self._k > 1:
                 re_norm = 2. / lambda_max
-
                 h = unnLaplacian(X_0, D_sqrt, graph)
 
                 X_1 = - re_norm * h + X_0 * (re_norm - 1)
@@ -112,6 +123,6 @@ class Cheb_Conv(nn.Module):
 
             # forward pass
             graph.ndata['h'] = Xt
-            graph.apply_nodes(func=lin_layer)
+            graph.apply_nodes(func=self.apply_mod)
 
             return graph.ndata.pop('h')
